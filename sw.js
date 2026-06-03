98605dd7537bf093d6ee6650769f6fae8b5c7396
Reuben8080 <92396053+Reuben8080@users.noreply.github.com>
2026-06-03 12:46:09 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'operator-v1';
+
+const PRECACHE_ASSETS = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/icons/icon-192.png',
+    '/icons/icon-512.png'
+]
+
+self.addEventListener('install', event => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll(PRECACHE_ASSETS);
+    })());
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        const cachedResponse = await cache.match(event.request);
+        if (cachedResponse !== undefined) {
+            return cachedResponse;
+        } else {
+            const networkResponse = await fetch(event.request);
+            if (networkResponse.ok) {
+                cache.put(event.request, networkResponse.clone());
+            }
+            return networkResponse;
+        }
+    })());
+});
